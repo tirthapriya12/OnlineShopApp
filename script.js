cbe9cf2f5f3277db67cbe2b8be5e642155cb7845
unknown <tirthapriya.banerjee@LMKOLPC030.learningmatekol.local>
2017-05-12 19:10:22 +0530

    update

--- script.js
+++ script.js
@@ -4,44 +4,84 @@ var dataURL = "data/data.json";
 OnlineShopApp = angular.module('OnlineShop', ['ui.router']);
 
 
-OnlineShopApp.controller('HomeCtrl', ['$scope', 'FetchData','CatchItem','Cart', function ($scope, FetchData , CatchItem,Cart) {
+OnlineShopApp.controller('HomeCtrl', ['$scope', 'FetchData', 'CatchItem', 'Cart', function ($scope, FetchData, CatchItem, Cart) {
 
-    //  $scope.brand=[];
+    
+        $scope.CartItems = [];
+
 
     FetchData.getData().success(function (data) {
         $scope.Product = data.productInfoList;
-        
+
     }); //gets all the data on page load
 
 
-    // $scope.$watch('price',function(value){
+   
+
+    $scope.sendItem = function (item) {
+
+        // $scope.itemId= obj
+
+        return {
+            itemid: item.productBaseInfo.productAttributes.productUrl.split('?pid=')[1].split('&')[0],
+            title: item.productBaseInfo.productAttributes.title
+        };
+
+    };  //sets item name and productid in urlparams 
 
 
-    // });
-    // $scope.getCurrentItem=function(item)
-    // {
-    //         $scope.item=item;
-    // };
+
+    $scope.getCount = function () {
 
 
-      $scope.sendItem=function(item){
-           
-            // $scope.itemId= obj
+        return Cart.getItemCount();
+    } //shows item count in cart
+
+    
+    // $scope.getCartItems = function () {
+
+    //     var C_items = [];
+
+    //    var a = Cart.getItems();
+    //    for(i in a)
+    //    {
+    //        C_items.push(a[i]);
+    //    }
+
+    //     for (var i = 0; i < C_items.length; i++) {
+    //         var count=1;
+    //         for (var j = i + 1; j < C_items.length; j++) {
+
+    //                 if(C_items[i].offset.split(':')[2]==C_items[j].offset.split(':')[2])
+    //                 {   
+    //                     count++;
+    //                     C_items[j].count=count;
+    //                     $scope.CartItems[i]=C_items[j];
+    //                     C_items.splice(j,1);
+    //                 }
+    //                 else{
+    //                      C_items[j].count=count;
+    //                     $scope.CartItems[i]=C_items[j];
+    //                 }
+                    
+    //         }
+    //         console.log($scope.CartItems);
             
-            return {
-                itemid:item.productBaseInfo.productAttributes.productUrl.split('?pid=')[1].split('&')[0],
-                title: item.productBaseInfo.productAttributes.title
-            };
 
-      };  //sets item name and productid in urlparams 
+    //     }
 
 
+    //     return $scope.CartItems;
+    // };//show items in cart
 
-      $scope.getCount = function() {
-            
-          
-          return Cart.getItemCount();
-      } //shows items in cart
+    $scope.getCartItems = function () {
+
+                 $scope.CartItems=Cart.getItems();
+
+                 return $scope.CartItems;
+                 
+    };
+
 
 
 }]);
@@ -56,8 +96,8 @@ OnlineShopApp.filter('FilterByName', function () {
 
             angular.forEach(items, function (item) {
 
-                var title=item.productBaseInfo.productAttributes.title.toLowerCase();
-                a=a.toLowerCase();
+                var title = item.productBaseInfo.productAttributes.title.toLowerCase();
+                a = a.toLowerCase();
                 if (title.indexOf(a) >= 0) {
                     output.push(item);
                 }
@@ -138,14 +178,14 @@ OnlineShopApp.filter('FilterByPrice', function () {
 OnlineShopApp.filter('FilterByBrand', function () {
 
     return function (items, Brand) {
-        var output = [], counter = 0,length=0;
+        var output = [], counter = 0, length = 0;
         if (typeof (Brand) != 'undefined') {
 
             for (i in Brand) {
                 if (!Brand[i]) {
                     counter++;
                 }
-                    length++;
+                length++;
             }
 
 
@@ -187,55 +227,63 @@ OnlineShopApp.filter('FilterByBrand', function () {
 
 });
 
-OnlineShopApp.controller('DescViewCtrl',['$scope','CatchItem','Cart','$stateParams', 'FetchData', function ($scope, CatchItem, Cart, $stateParams, FetchData) {
+OnlineShopApp.controller('DescViewCtrl', ['$scope', 'CatchItem', 'Cart', '$stateParams', 'FetchData', function ($scope, CatchItem, Cart, $stateParams, FetchData) {
 
-    
+
 
     FetchData.getData().success(function (data) {
-        
+
         $scope.Product = data.productInfoList;
-    
-    }); 
 
-   $scope.item=CatchItem.getThisItem($stateParams.itemid,$scope.Product); //get the required item for corresponding productid
-      
-      
-      $scope.addToCart=function(obj){
-                  
-                    Cart.setItem(obj);
-   
+    });
 
-      };
+    $scope.item = CatchItem.getThisItem($stateParams.itemid, $scope.Product); //get the required item for corresponding productid
 
 
- 
+    $scope.addToCart = function (obj) {
+
+        Cart.setItem(obj);
+
+
+    };
+
+
+
 
 }]);
 
-OnlineShopApp.config(function($stateProvider,$urlRouterProvider){
-//console.log($scope.Product.productBaseInfo.productAttributes.productUrl.split('?pid=')[1]);
- var home={
-    name:'home',
-    url:'/home',
-    constroller: 'HomeCtrl',
-   
+OnlineShopApp.config(function ($stateProvider, $urlRouterProvider) {
+    //console.log($scope.Product.productBaseInfo.productAttributes.productUrl.split('?pid=')[1]);
+    var home = {
+        name: 'home',
+        url: '/home',
+        constroller: 'HomeCtrl',
 
 
- };
 
-  var desc={
-     
-     name:'desc',
-     url:'/desc/:itemid/:title',
-     templateUrl:'product-desc.html',
-     controller: 'DescViewCtrl',
-     
-    
+    };
 
-  };
+    var desc = {
 
-   $stateProvider.state(desc);
-   $stateProvider.state(home);
-   $urlRouterProvider.otherwise("/home");
+        name: 'desc',
+        url: '/desc/:itemid/:title',
+        templateUrl: 'product-desc.html',
+        controller: 'DescViewCtrl',
 
+
+
+    };
+
+    $stateProvider.state(desc);
+    $stateProvider.state(home);
+    $urlRouterProvider.otherwise("/home");
+
+});
+
+OnlineShopApp.directive('checkout', function () {
+    return {
+        restrict: 'E',
+        transclude: 'true',
+        templateUrl: 'checkout.html'
+    };
 });
--- serviceCall.js
+++ serviceCall.js
@@ -10,7 +10,7 @@ OnlineShopApp.factory('FetchData', ['$http', function ($http) {
 
 
 
-OnlineShopApp.factory('CatchItem',  function () {
+OnlineShopApp.factory('CatchItem', function () {
 
     var item = {};
 
@@ -19,24 +19,24 @@ OnlineShopApp.factory('CatchItem',  function () {
             item = obj;
 
         },
-        getThisItem: function (itemid,Data) {
+        getThisItem: function (itemid, Data) {
 
 
-           
-                 for (var i = 0; i < Data.length; i++) {
+
+            for (var i = 0; i < Data.length; i++) {
                 if (Data[i].productBaseInfo.productAttributes.productUrl.indexOf(itemid) >= 0) {
-                    item=Data[i];
+                    item = Data[i];
                     console.log(item);
                 }
 
             }
             return item;
-            }//get data from database
+        }//get data from database
 
-           
-        };
 
-    
+    };
+
+
 
 });
 
@@ -54,8 +54,43 @@ OnlineShopApp.factory('Cart', function () {
             return Items.length;
         },
         setItem: function (object) {
+            var count = 1,flag=0;
+            if (Items.length == 0) {
+                Items.push(object);
+            }
+            else {
 
-            Items.push(object);
+
+                for (i in Items) {
+
+                    if (Items[i].offset.split(':')[2] == object.offset.split(':')[2]) {
+
+                        if (Items[i].count) {
+                            count = Items[i].count;
+                        }
+                        count++;
+
+                        Items[i].count = count;
+                        flag = 1;
+                        console.log(Items);
+                        break;
+
+                    }
+                }
+
+                if(flag!=1)
+                {
+                         Items.push(object);
+                }
+               
+
+
+
+
+
+            }
+
+            //Items.push(object);
 
             return true;
         }
